12eca33c7ac086af01d624a43c50314a9dfdfeb9
IslamTamjid <towhidulislam24@protonmail.com>
2023-11-07 00:09:21 +0600

    comments added

--- Task_101.m
+++ Task_101.m
@@ -2,8 +2,12 @@ a = input('Enter first matrix: ');
 b = input('Enter second matrix: ');
 r = rows(b);
 c = columns(a);
+
+%Max value of first matrix
 d = max(a);
 fprintf('Max value of A matrix: %d\n', d);
+
+%Matrix Multiplication
 if r == c
   res = a*b;
   fprintf('Product of A and B: \n');
